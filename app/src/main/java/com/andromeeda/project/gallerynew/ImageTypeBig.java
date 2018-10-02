d2c5d5ab0cf7b7ec9670a11601f471937306cecf
Aswin Bhim Nath <aswismail@gmail.com>
2018-10-02 13:04:18 +0530

    Image Classes

---
+++ app/src/main/java/com/andromeeda/project/gallerynew/ImageTypeBig.java
@@ -0,0 +1,33 @@
+package com.andromeeda.project.gallerynew;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.mindorks.placeholderview.Animation;
+import com.mindorks.placeholderview.PlaceHolderView;
+import com.mindorks.placeholderview.annotations.Animate;
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.View;
+
+/**
+ * Created by Aswin on 02-Oct-18.
+ */
+@Animate(Animation.ENTER_LEFT_DESC)
+@NonReusable
+@Layout(R.layout.gallery_item_big)
+public class ImageTypeBig {
+
+    @View(R.id.imageView)
+    private ImageView imageView;
+
+    private String mUlr;
+    private Context mContext;
+    private PlaceHolderView mPlaceHolderView;
+
+    public ImageTypeBig(Context context, PlaceHolderView placeHolderView, String ulr){
+        mContext = context;
+        mPlaceHolderView = placeHolderView;
+        mUlr = ulr;
+    }
+}
